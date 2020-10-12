1d7a3f168947691cdd00cc079b6ea16219b2e28e
J K <jkim65@buffalo.edu>
2020-10-12 22:47:50 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,11 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
+
   end
 end
 
 
 #add columns to students table
 # define change method
+# use add_column Active Record method
